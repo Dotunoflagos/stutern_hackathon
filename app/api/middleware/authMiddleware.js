a05f166343bf9d10a9734f0ce07edb6880dd6621
Balogun Adedotun <adedotun_balogun@yahoo.com>
2023-11-24 15:07:25 +0100

    dissabled auth

--- app/api/middleware/authMiddleware.js
+++ app/api/middleware/authMiddleware.js
@@ -1,22 +1,22 @@
 const jwt = require('jsonwebtoken');
 
 exports.authenticateToken = (req, res, next) => {
-  const token = req.cookies.Auth || req.cookies.signup || false;
+  // const token = req.cookies.Auth || req.cookies.signup || false;
 
-  if (!token) {
-    console.log('Token not found in the request header');
-    return res.status(401).json({ message: 'Authentication failed' });
-  }
+  // if (!token) {
+  //   console.log('Token not found in the request header');
+  //   return res.status(401).json({ message: 'Authentication failed' });
+  // }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      console.log('Error verifying token:', err);
-      return res.status(403).json({ message: 'Invalid token' });
-    }
+  // jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+  //   if (err) {
+  //     console.log('Error verifying token:', err);
+  //     return res.status(403).json({ message: 'Invalid token' });
+  //   }
 
-    req.userId = decoded.userId;
-    next();
-  });
-  // req.userId = req.body.userId2;
-  // next();
+  //   req.userId = decoded.userId;
+  //   next();
+  // });
+  req.userId = req.body.userId2;
+  next();
 };
