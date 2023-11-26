333b621a35ec0b398ee8b978c184f5886246a965
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2023-11-26 18:41:06 +0100

    Update Dashboard.tsx

--- app/client/src/pages/Dashboard/Dashboard.tsx
+++ app/client/src/pages/Dashboard/Dashboard.tsx
@@ -5,10 +5,12 @@ import { BsCashStack } from "react-icons/bs";
 import { CiClock2 } from "react-icons/ci";
 import ReactApexChart from "react-apexcharts";
 import {
+  useGetAllInvoice,
   useGetCompletedInvoice,
   useGetPendingInvoice,
   useGetTotalInvoice,
 } from "../../services/query/invoice-manager";
+import { Loader } from "../../components/WithSuspense";
 
 const Dashboard = () => {
   const { data, isLoading } = useGetTotalInvoice();
@@ -18,6 +20,10 @@ const Dashboard = () => {
 
   const { data: completedData, isLoading: completeLoading } =
     useGetCompletedInvoice();
+
+  const { data: allInvoiceList, isLoading: invoiceLoading } =
+    useGetAllInvoice();
+  // console.log(allInvoiceList);
 
   const formattedTotal = (data?.totalAmount / 100).toLocaleString();
   const formattedPending = (pendingData?.pendingData / 100).toLocaleString();
@@ -45,209 +51,219 @@ const Dashboard = () => {
       icon: CiClock2,
     },
   ];
-
-  const invoices = [
-    {
-      name: "John Doe",
-    },
-    {
-      name: "Adeyemi Sigurdsson",
-    },
-    {
-      name: "Philly Guy",
-    },
-    {
-      name: "Malone Kramer",
-    },
-    {
-      name: "Steve Kenny",
-    },
-  ];
   return (
     <>
-      {/* <Header name={name} /> */}
-      <Box mt="2rem">
-        <Box>
-          <Flex
-            justifyContent={"space-between"}
-            align={"center"}
-            gap="10px"
-            flexWrap={"wrap"}
-          >
-            {cards.map((card, index) => (
-              <Box
-                key={index}
-                borderRadius={"12px"}
-                border="1px solid #DEE2E6"
-                p="12px"
-                height="140px"
-                width={["100%", "100%", "100%", "32%"]}
-              >
-                <Flex alignItems={"flex-end"} justifyContent={"space-between"}>
-                  <Flex flexDirection={"column"} gap="10px">
-                    <Flex
-                      w="40px"
-                      h="40px"
-                      borderRadius="50%"
-                      border="1px solid #E4E7EC"
-                      justifyContent={"center"}
-                      alignItems={"center"}
-                    >
-                      <Icon
-                        fontSize={"20px"}
-                        color={"#868E96"}
-                        as={card.icon}
-                      />
-                    </Flex>
-                    <Text color="#475367" fontSize="12px" whiteSpace={"nowrap"}>
-                      {card.name}
-                    </Text>
-                    <Text fontSize={"18px"} fontWeight={"600"}>
-                      NGN {card.amount}
-                    </Text>
-                  </Flex>
-                  <Flex
-                    borderRadius="10px"
-                    bg="#E7F6EC"
-                    fontSize="12px"
-                    color="#036B26"
-                    fontWeight="600"
-                    p="1"
-                    alignItems={"center"}
-                    gap="2px"
-                  >
-                    <Icon as={TbGraph} /> +{card.percent} Today
-                  </Flex>
-                </Flex>
-              </Box>
-            ))}
-          </Flex>
-        </Box>
-
-        <Box mt="30px">
-          <Flex
-            justifyContent={"space-between"}
-            // align={"center"}
-            gap="10px"
-            flexWrap={"wrap"}
-          >
-            <Box
-              borderRadius={"12px"}
-              border="1px solid #DEE2E6"
-              p="16px"
-              minH="140px"
-              width={["100%", "100%", "100%", "48%"]}
+      {invoiceLoading || completeLoading || pendingLoading || isLoading ? (
+        <Loader />
+      ) : (
+        <Box mt="2rem">
+          <Box>
+            <Flex
+              justifyContent={"space-between"}
+              align={"center"}
+              gap="10px"
+              flexWrap={"wrap"}
             >
-              <Flex justifyContent={"space-between"} alignItems={"center"}>
-                <Text fontSize="18px" fontWeight="600">
-                  Invoice Payment History
-                </Text>
-                <Text color="#5C7CFA " fontSize="14px" fontWeight="600">
-                  See all
-                </Text>
-              </Flex>
-              <Box mt="20px">
-                {invoices.map((inv, index) => (
+              {cards.map((card, index) => (
+                <Box
+                  key={index}
+                  borderRadius={"12px"}
+                  border="1px solid #DEE2E6"
+                  p="12px"
+                  height="140px"
+                  width={["100%", "100%", "100%", "32%"]}
+                >
                   <Flex
+                    alignItems={"flex-end"}
                     justifyContent={"space-between"}
-                    alignItems={"center"}
-                    key={index}
-                    mb="4"
                   >
-                    <Flex gap="10px">
+                    <Flex flexDirection={"column"} gap="10px">
                       <Flex
                         w="40px"
                         h="40px"
                         borderRadius="50%"
-                        bg="#94D82D"
-                        color="white"
+                        border="1px solid #E4E7EC"
                         justifyContent={"center"}
                         alignItems={"center"}
                       >
-                        JD
+                        <Icon
+                          fontSize={"20px"}
+                          color={"#868E96"}
+                          as={card.icon}
+                        />
                       </Flex>
-                      <Box>
-                        <Text fontSize={"14px"} fontWeight={"500"}>
-                          {inv.name}
-                        </Text>
-                        <Text
-                          fontSize={"12px"}
-                          color={"#667185"}
-                          fontWeight={"400"}
-                        >
-                          Invoice ID #20192 10 hours ago
-                        </Text>
-                      </Box>
+                      <Text
+                        color="#475367"
+                        fontSize="12px"
+                        whiteSpace={"nowrap"}
+                      >
+                        {card.name}
+                      </Text>
+                      <Text fontSize={"18px"} fontWeight={"600"}>
+                        NGN {card.amount}
+                      </Text>
                     </Flex>
-                    <Text fontSize={"14px"} fontWeight={"600"}>
-                      $1,800
-                    </Text>
+                    <Flex
+                      borderRadius="10px"
+                      bg="#E7F6EC"
+                      fontSize="12px"
+                      color="#036B26"
+                      fontWeight="600"
+                      p="1"
+                      alignItems={"center"}
+                      gap="2px"
+                    >
+                      <Icon as={TbGraph} /> +{card.percent} Today
+                    </Flex>
                   </Flex>
-                ))}
-              </Box>
-            </Box>
-            <Box
-              borderRadius={"12px"}
-              border="1px solid #DEE2E6"
-              p="12px"
-              minH="140px"
-              width={["100%", "100%", "100%", "48%"]}
+                </Box>
+              ))}
+            </Flex>
+          </Box>
+
+          <Box mt="30px">
+            <Flex
+              justifyContent={"space-between"}
+              // align={"center"}
+              gap="10px"
+              flexWrap={"wrap"}
             >
-              <Flex justifyContent={"space-between"} alignItems={"center"}>
-                <Text fontSize="18px" fontWeight="600">
-                  Payment Analytics
-                </Text>
-                <Text color="#5C7CFA " fontSize="14px" fontWeight="600">
-                  See all
-                </Text>
-              </Flex>
-              <Box mt="20px">
-                <ReactApexChart
-                  options={{
-                    chart: {
-                      type: "area",
-                    },
-                    stroke: {
-                      curve: "smooth",
-                    },
-                    fill: {
-                      type: "gradient",
-                      gradient: {
-                        shadeIntensity: 1,
-                        opacityFrom: 1,
-                        opacityTo: 1,
-                        stops: [100, 90, 100],
-                      },
-                    },
-                    dataLabels: {
-                      enabled: false,
-                    },
-                    xaxis: {
-                      categories: [
-                        " Jan",
-                        " Feb",
-                        " Mar",
-                        " Apr",
-                        " May",
-                        " Jun",
-                        " Jul",
-                      ],
-                    },
-                  }}
-                  series={[
-                    {
-                      name: "Series 1",
-                      data: [90, 40, 65, 70, 20, 40, 50],
-                    },
-                  ]}
-                  type="area"
-                  height={300}
-                />
+              <Box
+                borderRadius={"12px"}
+                border="1px solid #DEE2E6"
+                p="16px"
+                minH="140px"
+                width={["100%", "100%", "100%", "48%"]}
+              >
+                <Flex justifyContent={"space-between"} alignItems={"center"}>
+                  <Text fontSize="18px" fontWeight="600">
+                    Invoice Payment History
+                  </Text>
+                  <Text color="#5C7CFA " fontSize="14px" fontWeight="600">
+                    See all
+                  </Text>
+                </Flex>
+                {allInvoiceList?.length > 0 ? (
+                  <Box mt="20px">
+                    {allInvoiceList?.map((inv: any, index: any) => (
+                      <Flex
+                        justifyContent={"space-between"}
+                        alignItems={"center"}
+                        key={index}
+                        mb="6"
+                      >
+                        <Flex gap="10px">
+                          <Flex
+                            w="40px"
+                            h="40px"
+                            borderRadius="50%"
+                            bg="#94D82D"
+                            color="white"
+                            justifyContent={"center"}
+                            alignItems={"center"}
+                          >
+                            {inv?.firstname[0]}
+                            {inv?.lastname[0]}
+                          </Flex>
+                          <Box>
+                            <Text fontSize={"14px"} fontWeight={"500"}>
+                              {inv.firstname} {inv.lastname}
+                            </Text>
+                            <Text
+                              fontSize={"12px"}
+                              color={"#667185"}
+                              fontWeight={"400"}
+                            >
+                              Invoice ID: {inv?.invoiceNumber}
+                            </Text>
+                          </Box>
+                        </Flex>
+                        <Text fontSize={"14px"} fontWeight={"600"}>
+                          NGN {inv?.amount / 100}
+                        </Text>
+                      </Flex>
+                    ))}
+                  </Box>
+                ) : (
+                  <Flex
+                    justifyContent={"center"}
+                    alignItems={"center"}
+                    h="100%"
+                  >
+                    <Box textAlign={"center"}>
+                      <Text color="#344054" fontWeight="600">
+                        No data to show
+                      </Text>
+                      <Text color="#667185" fontSize="12px">
+                        Create a client profile and start sending invoices
+                      </Text>
+                    </Box>
+                  </Flex>
+                )}
               </Box>
-            </Box>
-          </Flex>
+              <Box
+                borderRadius={"12px"}
+                border="1px solid #DEE2E6"
+                p="12px"
+                minH="140px"
+                width={["100%", "100%", "100%", "48%"]}
+              >
+                <Flex justifyContent={"space-between"} alignItems={"center"}>
+                  <Text fontSize="18px" fontWeight="600">
+                    Payment Analytics
+                  </Text>
+                  <Text color="#5C7CFA " fontSize="14px" fontWeight="600">
+                    See all
+                  </Text>
+                </Flex>
+                <Box mt="20px">
+                  <ReactApexChart
+                    options={{
+                      chart: {
+                        type: "area",
+                      },
+                      stroke: {
+                        curve: "smooth",
+                      },
+                      fill: {
+                        type: "gradient",
+                        gradient: {
+                          shadeIntensity: 1,
+                          opacityFrom: 1,
+                          opacityTo: 1,
+                          stops: [100, 90, 100],
+                        },
+                      },
+                      dataLabels: {
+                        enabled: false,
+                      },
+                      xaxis: {
+                        categories: [
+                          " Jan",
+                          " Feb",
+                          " Mar",
+                          " Apr",
+                          " May",
+                          " Jun",
+                          " Jul",
+                        ],
+                      },
+                    }}
+                    series={[
+                      {
+                        name: "Series 1",
+                        data: [90, 40, 65, 70, 20, 40, 50],
+                      },
+                    ]}
+                    type="area"
+                    height={300}
+                  />
+                </Box>
+              </Box>
+            </Flex>
+          </Box>
         </Box>
-      </Box>
+      )}
     </>
   );
 };
